1e2b38c5845a621d0da66d0f135957360d0bc00c
Kishan Jasani <kishanjasani007@yahoo.in>
2020-06-22 17:29:16 +0530

    Add night mode functionality in block

--- blocks/app/index.js
+++ blocks/app/index.js
@@ -1,3 +1,4 @@
 // Main File
 import './recipe-block';
 import './richtext-block';
+import './night-mode-block';
--- blocks/app/night-mode-block/index.js
+++ blocks/app/night-mode-block/index.js
@@ -3,25 +3,47 @@ import block_icons from '../icons/index';
 import btn_icon from './icon';
 import './editor.scss';
 
-const { registerBlockType }         =   wp.blocks;
-const { __ }                        =   wp.i18n;
-const { BlockControls }             =   wp.editor;
+const { registerBlockType }        =   wp.blocks;
+const { __ }                       =   wp.i18n;
+const { BlockControls }            =   wp.editor;
+const { Toolbar, Button, Tooltip } = wp.components;
 
 registerBlockType( 'udemy/night-mode', {
-    title:                              __( 'Night Mode', 'recipe' ),
-    description:                        __( 'Content with night mode.', 'recipe'),
-    category:                           'common',
-    icon:                               block_icons.wapuu,
+    title:       __( 'Night Mode', 'recipe' ),
+    description: __( 'Content with night mode.', 'recipe'),
+    category:    'common',
+    icon:        block_icons.wapuu,
     attributes: {
-
+		night_mode: {
+			type:    'boolean',
+			default: false
+		}
     },
     edit: ( props ) => {
         return (
             <div className={ props.className }>
                 <BlockControls>
-
+					<Toolbar>
+						<Tooltip text={ __( 'Night mode', 'recipe' ) }>
+							<Button className={ classnames(
+								'components-icon-button',
+								'components-toolbar__control',
+								{ 'is-active': props.attributes.night_mode }
+							) }
+							onClick = { () => {
+								props.setAttributes({
+									night_mode: !props.attributes.night_mode
+								})
+							}}>
+								{ btn_icon }
+							</Button>
+						</Tooltip>
+					</Toolbar>
                 </BlockControls>
-                <div className="content-example">
+                <div className={ classnames(
+					"content-example",
+					{ 'night': props.attributes.night_mode }
+				)}>
                     This is an example of a block with night mode.
                 </div>
             </div>
@@ -30,7 +52,10 @@ registerBlockType( 'udemy/night-mode', {
     save: ( props ) => {
         return (
             <div>
-                <div className="content-example">
+                <div className={ classnames(
+					"content-example",
+					{ 'night': props.attributes.night_mode }
+				)}>
                     This is an example of a block with night mode.
                 </div>
             </div>
